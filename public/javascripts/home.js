8319f3efada5a305a35b34eeb73b23430bb801ff
Sam Lerner <samlerner@sams-mbp.attlocal.net>
2017-05-19 08:43:41 -0400

    dfsdfs

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,4 +1,11 @@
 genomicData = [];
+var margins = {
+    top: 50,
+    bottom: 50,
+    left: 50,
+    right: 50
+};
+
 var parseGenomicData = function(callback) {
     var type = function(d) {
         d.CHR = +d.CHR;
@@ -47,14 +54,8 @@ var displayChart = function(gene) {
     removeExistingCharts();
 
     var buffer = 0.005;
-    var margins = {
-        top: 10,
-        bottom: 50,
-        left: 50,
-        right: 10
-    };
     var width = (+d3.select('body').node().getBoundingClientRect().width) - margins.left - margins.right,
-        height = 300 - margins.top - margins.bottom;
+        height = 400 - margins.top - margins.bottom;
 
     var upperBound = 0, lowerBound = 0;
     var data = [];
@@ -124,13 +125,14 @@ var displayChart = function(gene) {
     chart.selectAll('.point').data(data)
         .enter().append('circle')
         .attr('class', 'point')
+        .attr('bp', function(d) { return d.BP; })
         .attr('snp', function(d) { return d.RS; })
         .attr('freq', function(d) { return d.FREQ; })
         .attr('p', function(d) { return d.P; })
         .attr('scaledFreq', function(d) { return freqScale(d.FREQ); })
         .attr('id', function(d, i) { return 'snp' + i; })
         .attr('cx', function(d) { return x(d.BP/1000000); })
-        .attr('cy', function(d) { return y(-Math.log10(d.P)); })
+        .attr('cy', function(d) { return y(Math.random()); })
         .attr('r', function(d) { return freqScale(d.FREQ); });
     
     addAnnotationHover();
@@ -154,7 +156,7 @@ var addAnnotationHover = function() {
     });
 }
 
-var upPath = function(radius, width, height, sWidth, offset) {
+var annotationPath1 = function(radius, width, height, sWidth, offset) {
     return 'M ' + radius + ' 0 L ' + (width - radius) + ' 0 '
     + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + width + ' ' + radius
     + ' L ' + width + ' ' + (height - radius)
@@ -168,99 +170,45 @@ var upPath = function(radius, width, height, sWidth, offset) {
     + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + radius + ' ' + 0;
 }
 
-var downPath = function(radius, width, height, sWidth, offset) {
-    return 'M ' + radius + ' 0 L ' + (width/2 - sWidth/2) + ' ' + 0
-    + ' L ' + (width/2) + ' ' + (-offset/2)
-    + ' L ' + (width/2 + sWidth/2) + ' ' + 0
-    + ' L ' + (width - radius) + ' ' + 0
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + width + ' ' + radius
-    + ' L ' + width + ' ' + (height - radius)
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + (width - radius) + ' ' + height
-    + ' L ' + radius + ' ' + height
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + 0 + ' ' + (height - radius)
-    + ' L ' + 0 + ' ' + radius
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + radius + ' ' + 0;
-}
-
-var rightPath = function(radius, width, height, sHeight, offset) {
-    return 'M ' + radius + ' 0 L ' + (width - radius) + ' 0 '
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + width + ' ' + radius
-    + ' L ' + width + ' ' + (height - radius)
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + (width - radius) + ' ' + height
-    + ' L ' + radius + ' ' + height
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + 0 + ' ' + (height - radius)
-    + ' L ' + 0 + ' ' + (height/2 + sHeight/2)
-    + ' L ' + (-offset/2) + ' ' + (height/2)
-    + ' L ' + 0 + ' ' + (height/2 - sHeight/2)
-    + ' L ' + 0 + ' ' + radius
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + radius + ' ' + 0;
-}
-
-var leftPath = function(radius, width, height, sHeight, offset) {
-    return 'M ' + radius + ' 0 L ' + (width - radius) + ' 0 '
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + width + ' ' + radius
-    + ' L ' + width + ' ' + (height/2 - sHeight/2)
-    + ' L ' + (width + offset/2) + ' ' + (height/2)
-    + ' L ' + width + ' ' + (height/2 + sHeight/2)
-    + ' L ' + width + ' ' + (height - radius)
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + (width - radius) + ' ' + height
-    + ' L ' + radius + ' ' + height
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + 0 + ' ' + (height - radius)
-    + ' L ' + 0 + ' ' + radius
-    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + radius + ' ' + 0;
+var annotationPath2 = function(radius, width, height, sWidth, offset) {
+    return 'M ' + radius + ' ' + offset + ' L ' + (width/2 - sWidth/2) + ' ' + offset
+    + ' L ' + (width/2) + ' ' + (offset/2)
+    + ' L ' + (width/2 + sWidth/2) + ' ' + offset
+    + ' L ' + (width - radius) + ' ' + offset
+    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + width + ' ' + (radius+offset)
+    + ' L ' + width + ' ' + (height - radius + offset)
+    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + (width - radius) + ' ' + (height + offset)
+    + ' L ' + radius + ' ' + (height + offset)
+    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + 0 + ' ' + (height - radius + offset)
+    + ' L ' + 0 + ' ' + (radius + offset)
+    + ' A ' + radius + ' ' + radius + ' 0 0 1 ' + radius + ' ' + offset;
 }
 
 var addAnnotationForSNP = function(id) {
     var point = d3.select('#' + id);
     var annotationWidth = 150, annotationHeight = 100;
-    var pointX = point.attr('cx'), pointY = point.attr('cy');
+    var pointX = (+point.attr('cx')), pointY = (+point.attr('cy'));
 
     var fontSize = 14,
-        radius = 10, triangleWidth = 20, containerOffset = 20,
-        textInset = 15, interlineSpacing = 5;
-    var annotationX = pointX - annotationWidth/2, 
-        annotationY = pointY - annotationHeight - containerOffset;
+        radius = 10, triangleWidth = 20, offset = 20,
+        textInset = { x: 15, y: 15 }, interlineSpacing = 5;
+    var annotationX = pointX - annotationWidth/2 + margins.left, 
+        annotationY = pointY - annotationHeight - offset + margins.top;
 
-    var offsetContainer = d3.select('.chart').select('#offsetContainer');
     var path = '';
-    if (annotationY >= 0) {
-        if (annotationX >= 0) {
-            if (annotationX > (+offsetContainer.attr('width'))) {
-                annotationX = pointX - 2*containerOffset - annotationWidth;
-                annotationY = pointY - annotationHeight/2;
-                path = leftPath(radius, annotationWidth, annotationHeight,
-                triangleWidth, containerOffset);
-            } else {
-                path = upPath(radius, annotationWidth, annotationHeight,
-                triangleWidth, containerOffset);
-            }
-        } else {
-            annotationX = pointX + 2*containerOffset;
-            annotationY = pointY - annotationHeight/2;
-            path = rightPath(radius, annotationWidth, annotationHeight,
-            triangleWidth, containerOffset);
-        }
+    if (annotationY < 0) {
+        textInset.y += offset;
+        annotationY = pointY + margins.top;
+        path = annotationPath2(radius, annotationWidth, annotationHeight,
+            triangleWidth, offset);
+        offset /= 2;
     } else {
-        annotationY = pointY + 2*containerOffset;
-        if (annotationX >= 0) {
-            if (annotationX > (+offsetContainer.attr('width'))) {
-                annotationX = pointX + 2*containerOffset;
-                annotationY = pointY - annotationHeight/2;
-                path = rightPath(radius, annotationWidth, annotationHeight,
-                triangleWidth, containerOffset);
-            } else {
-                path = downPath(radius, annotationWidth, annotationHeight,
-                triangleWidth, containerOffset);
-            }
-        } else {
-            annotationX = pointX - 2*containerOffset - annotationWidth;
-            annotationY = pointY - annotationHeight/2;
-            path = leftPath(radius, annotationWidth, annotationHeight,
-            triangleWidth, containerOffset);
-        }
+        path = annotationPath1(radius, annotationWidth, annotationHeight,
+            triangleWidth, offset);
     }
 
-    var annotation = offsetContainer.append('g')
+    var annotation = d3.select('.chart').append('g')
+        .attr('class', 'annotation')
         .attr('id', 'annotation' + id)
         .attr('transform', 'translate(' + annotationX + ',' + annotationY + ')')
         .attr('width', annotationWidth).attr('height', annotationHeight);
@@ -274,33 +222,28 @@ var addAnnotationForSNP = function(id) {
 
     var rsText = annotation.append('text')
         .attr('class', 'label')
-        .attr('x', textInset).attr('y', textInset)
+        .attr('x', annotationWidth/2).attr('y', textInset.y)
         .attr('dy', 2)
         .style('font-weight', '600')
-        .style('text-align', 'center')
-        .text(point.attr('snp'));
+        .text(point.attr('snp'));   
 
     var freqText = annotation.append('text')
         .attr('class', 'label')
-        .attr('x', textInset).attr('y', (+rsText.attr('dy')) + (+rsText.attr('y')) + fontSize + interlineSpacing)
-        .attr('width', rsText.attr('width')).attr('height', rsText.attr('height'))
+        .attr('x', annotationWidth/2).attr('y', (+rsText.attr('dy')) + (+rsText.attr('y')) + fontSize + interlineSpacing)
         .text('Frequency: ' + point.attr('freq'));
 
     var pText = annotation.append('text')
         .attr('class', 'label')
-        .attr('x', textInset).attr('y', (+freqText.attr('y')) + fontSize + interlineSpacing)
-        .attr('width', freqText.attr('width')).attr('height', freqText.attr('height'))
+        .attr('x', annotationWidth/2).attr('y', (+freqText.attr('y')) + fontSize + interlineSpacing)
         .text('p: ' + point.attr('p'));
 
-    var effText = annotation.append('text')
+    var bpText = annotation.append('text')
         .attr('class', 'label')
-        .attr('x', textInset).attr('y', (+pText.attr('y')) + fontSize + interlineSpacing)
-        .attr('width', pText.attr('width')).attr('height', pText.attr('height'))
-        .text('Effect: ' + (-Math.log10((+point.attr('p'))).toFixed(3)));
+        .attr('x', annotationWidth/2).attr('y', (+pText.attr('y')) + fontSize + interlineSpacing)
+        .text('Location: ' + point.attr('bp'));
 }
 
 var removeAnnotationForSNP = function(id) {
     var point = d3.select('#' + id);
-    d3.select('.chart').select('#offsetContainer')
-        .select('#annotation' + id).remove();
+    d3.select('.chart').select('#annotation' + id).remove();
 }
