095cef8e522b49a1eb3e54e69d842158b9d61406
Sam Lerner <lerner98@gmail.com>
2017-07-19 10:54:57 -0400

    merge commit

--- app.js
+++ app.js
@@ -5,24 +5,7 @@ var dbmanager = require('./DBManager.js');
 var exec = require('child_process').exec;
 
 var app = express();
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-
-/*var srcPath = __dirname + '/scss', destPath = __dirname + '/public/stylesheets';
-app.use('/stylesheets', sassMiddleware({
-        src: srcPath,
-        dest: destPath,
-        debug: true,
-        outputStyle: 'compressed'
-    })
-);*/
 app.use(express.static(path.join(__dirname, 'public')));
-app.all('*', allowCrossDomain);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/public/home.html"));
