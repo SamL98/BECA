fd35206cb8e381da75957d940407943ccb565e34
Sam Lerner <lerner98@gmail.com>
2017-11-12 08:48:01 -0500

    added trait table styling

--- public/javascripts/snp.js
+++ public/javascripts/snp.js
@@ -7,18 +7,26 @@ var addSNPInfo = function(snps) {
     let chartRect = rectFor('#chart-container')
 
     let div = d3.select('#assoc-container')
-        .style('background-color', 'lightgray')
+        .style('border', '2.5px solid black')
+        .style('margin-top', '20px')
 
-    div.append('p').text(currSnp.name)
+    div.append('h3')
+        .style('width', '100%')
+        .style('text-align', 'center')
+        .style('font-family', 'arial')
+        .text(currSnp.name)
 
     let table = div.append('table')
         .attr('id', 'traits-table')
+        .style('width', '100%')
+
     let thead = table.append('thead')
     let tbody = table.append('tbody')
 
     let headers = ['Pubmed ID', 'Cohort', 'Trait', 'P-Value']
     thead.append('tr').selectAll('th')
         .data(headers).enter().append('th')
+        .style('font-family', 'arial')
         .text(h => { return h })
 
     let rows = tbody.selectAll('tr')
@@ -31,5 +39,7 @@ var addSNPInfo = function(snps) {
                 return {column: headers[i], value: vals[i]}
             })
         }).enter()
-        .append('td').text(d => { return d.value })
+        .append('td')
+            .style('font-family', 'arial')
+            .text(d => { return d.value })
 }
